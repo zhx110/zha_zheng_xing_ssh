ae94ade63f86612676da0eb856f7f81ec9c0e506
zha_zheng_xing <1006814459@qq.com>
2016-06-18 02:59:30 +0800

    第一个demo，push到gitbhu

---
+++ demo_git/demo_git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo_git
+//
+//  Created by zha on 16/6/18.
+//  Copyright © 2016年 zha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ demo_git/demo_git/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  demo_git
+//
+//  Created by zha on 16/6/18.
+//  Copyright © 2016年 zha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
